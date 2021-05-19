ba8b170b02277302da24e54dbeb92ea055bbcd2d
vikasjain <vikasjain@localhost>
2021-05-19 09:27:31 +0530

    Commit#3 Added PostMapping

--- src/main/java/com/demo/aws/sqs/SpringbootAwsSqsExeApplication.java
+++ src/main/java/com/demo/aws/sqs/SpringbootAwsSqsExeApplication.java
@@ -1,5 +1,9 @@
 package com.demo.aws.sqs;
 
+
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,11 +12,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @SpringBootApplication(exclude = {ContextStackAutoConfiguration.class})
 @Async
@@ -29,10 +35,30 @@ public class SpringbootAwsSqsExeApplication {
     @Value("${cloud.aws.end-point.uri}")
     private String endpoint;
 
-    @GetMapping("/send/{message}")
-    public void sendMessageToQueue(@PathVariable String message) {
-        queueMessagingTemplate.send(endpoint, MessageBuilder.withPayload(message).build());
-        
+    
+    @PostMapping(value = "/posts")
+    public ResponseEntity<Msg> createPost(HttpServletRequest request,
+                                        UriComponentsBuilder uriComponentsBuilder) {
+    	
+    	try {
+//    		Msg m = new Msg();
+//        	m.setMessage(request.getParameter("msg"));
+        	queueMessagingTemplate.send(endpoint, MessageBuilder.withPayload(request.getParameter("msg")).build());							
+        	
+        	
+//        	UriComponents uriComponents =
+//                    uriComponentsBuilder.path("/posts/{id}").buildAndExpand(m.getId());
+//            URI location = uriComponents.toUri();
+           return ResponseEntity.created(null).build();
+            
+    	}
+    	catch(Exception e) {
+    		e.printStackTrace();
+    		return null;
+    	}
+    	
+
+    	
     }
     
 
